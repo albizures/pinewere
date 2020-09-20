59a12db197cadeb2853156ff48125a890eae885a
Jose Albizures <albizures3601@gmail.com>
2020-09-20 12:52:12 +0300

    use query params

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,35 @@
-import { css } from 'goober';
+import { css, glob } from 'goober';
 import { createElement, setStyles, px } from './utils/elements';
 import { getSelection } from './utils';
 
 let currentSelection: ReturnType<typeof getSelection>;
 
-const caretSize = 8;
 const color = '#FAE704';
+glob`
+	@keyframes flash {
+		0% {
+			background: ${color};
+		}
+		80% {
+			background: transparent
+		}
+	}
+`;
+
+const highlightedText = css`
+	background-color: transparent;
+	animation: 2s flash 1;
+	border: 1px dashed gray;
+`;
+
+enum QueryParams {
+	PATH = 'pinewere_path',
+	START = 'pinewere_start',
+	END = 'pinewere_end',
+}
+
+const caretSize = 8;
+
 const backdrop = createElement('div', {
 	className: css`
 		position: fixed;
@@ -98,7 +122,6 @@ const onBackdropClick = () => {
 
 const onSave = (event: MouseEvent) => {
 	hideMenu();
-	console.log('saving', currentSelection);
 
 	event.stopPropagation();
 
@@ -123,9 +146,84 @@ const addElements = () => {
 	);
 };
 
+const checkQueryParams = () => {
+	const urlParams = new URLSearchParams(window.location.search);
+	const path = urlParams.get(QueryParams.PATH);
+	const start = Number(urlParams.get(QueryParams.START));
+	const end = Number(urlParams.get(QueryParams.END));
+
+	if (!path || Number.isNaN(start) || Number.isNaN(end)) {
+		return;
+	}
+
+	const el = document.querySelector(path);
+
+	if (!el) {
+		return;
+	}
+
+	const { textContent } = el;
+	const textBefore = textContent.substring(0, start);
+	const text = textContent.substring(start, end);
+	const textAfter = textContent.substring(end);
+	const mark = createElement('mark', {
+		className: highlightedText,
+	});
+
+	const children = Array.from(el.children);
+	const hasElements = children.length > 0;
+	if (hasElements) {
+		console.log(text);
+		// console.log(el.);
+
+		children.reduce((index, child, currentIndex) => {
+			const startElement = text.indexOf(child.textContent);
+			const endElement = startElement + child.textContent.length;
+
+			console.log(index, startElement, text.substring(index, startElement));
+
+			mark.appendChild(
+				document.createTextNode(text.substring(index, startElement)),
+			);
+			mark.appendChild(child);
+
+			if (currentIndex === children.length - 1) {
+				console.log(
+					text.substring(endElement),
+					index,
+					startElement,
+					endElement,
+				);
+
+				mark.appendChild(document.createTextNode(text.substring(endElement)));
+			} else {
+				const nextChild = children[index + 1];
+				const startNextElement = text.indexOf(nextChild.textContent);
+				mark.appendChild(
+					document.createTextNode(text.substring(endElement, startNextElement)),
+				);
+			}
+
+			return endElement;
+		}, 0);
+	} else {
+		mark.appendChild(document.createTextNode(text));
+	}
+
+	el.scrollIntoView({
+		behavior: 'smooth',
+	});
+
+	el.innerHTML = '';
+	el.appendChild(document.createTextNode(textBefore));
+	el.appendChild(mark);
+	el.appendChild(document.createTextNode(textAfter));
+};
+
 const setup = () => {
 	addListeners();
 	addElements();
+	checkQueryParams();
 };
 
 setup();
